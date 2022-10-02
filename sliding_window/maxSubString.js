01fd2f44c972283802a2124fcf9f51c97ae4ec7d
--show-origin <satyadash792@gmail.com>
2022-10-02 13:09:03 +0530

    longest_subString

--- sliding_window/maxSubString.js
+++ sliding_window/maxSubString.js
@@ -2,25 +2,24 @@ function findLongestSubstring(str){
  if(!str)    
  return 0
  let obj={}
- let start=0
- let end=0
- let max=1
+ let startPointer=0
+ let endPointer=0
+ let max=1;
 for(let i=0;i<str.length;i++){
 if(obj[str[i]]){
-    start++
-}else{
-    max=Math.max(max, end-start+1)
-    obj[str[i]]=1 
-    end++
+    // as some element stored already if you donot use this startPointer will move backward
+    startPointer=Math.max(startPointer, obj[str[i]]) 
 }
-console.log(`value of i is ${i} and max is ${max}`)
+max=Math.max(max, endPointer-startPointer+1)
+obj[str[i]]=i+1;
+endPointer++
 }
 return max
 }
 
 console.log(findLongestSubstring('')) // 0
 console.log(findLongestSubstring('rithmschool')) // 7
-console.log(findLongestSubstring('thisisawesome')) // 6
+console.log(findLongestSubstring('thisisawesome')) // 6  
 console.log(findLongestSubstring('thecatinthehat')) // 7
 console.log(findLongestSubstring('bbbbbb')) // 1
 console.log(findLongestSubstring('longestsubstring')) // 8
