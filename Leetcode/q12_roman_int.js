33e0f354fa95e8e9f6d77c0f37d8313da2d39aa9
--show-origin <satyadash792@gmail.com>
2022-10-20 23:55:43 +0530

    roman_to_int

---
+++ Leetcode/q12_roman_int.js
@@ -0,0 +1,10 @@
+var romanToInt = function(s) {
+    let obj={
+        I:1,V:5,X:10,L:50,C:100,D:500,M:1000
+    }
+    let sum=0
+    for(let i=0;i<s.length;i++){
+        sum+=obj[s[i]]
+    }
+    return sum
+};
